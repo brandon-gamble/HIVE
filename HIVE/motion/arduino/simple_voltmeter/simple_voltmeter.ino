437006752f169022f5cc6dcbeba0ee80731d0d03
brandon <brandon05250@gmail.com>
2022-04-12 11:48:25 -0400

    added filter to adc read

--- HIVE/motion/arduino/simple_voltmeter/simple_voltmeter.ino
+++ HIVE/motion/arduino/simple_voltmeter/simple_voltmeter.ino
@@ -9,8 +9,9 @@ float R2 = 22000.0;
 float ref_voltage = 5.0;
 
 int adc_val = 0;
+float adc_filt = 0.0;
 
-int interval = 1000;
+int interval = 50;
 
 long currentMillis = 0;
 long previousMillis = 0;
@@ -26,6 +27,12 @@ void loop(){
 
         // get adc analog value
         adc_val = analogRead(ANALOG_IN_PIN);
+        adc_filt = adc_filt + (float(adc_val)- adc_filt)/32;
+        Serial.print(adc_val);
+        Serial.print(", ");
+        Serial.print(adc_filt);
+        Serial.print(", ");
+        Serial.println("");
 
         // compute voltage at the divider
         divider_voltage = adc_val/1024.0*ref_voltage;
@@ -34,8 +41,9 @@ void loop(){
         device_voltage = divider_voltage/(R2/(R1+R2));
 
         // print voltage
-        Serial.print(device_voltage);
-        Serial.println(" V");
+//        Serial.print(device_voltage);
+//        Serial.println(" V");
+
     }
 
 }
