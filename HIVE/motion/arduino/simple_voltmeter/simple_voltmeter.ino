f7cf3c8a7804b9adbefa08b669b01d23f1f73428
brandon <brandon05250@gmail.com>
2022-04-13 11:53:32 -0400

    added filtering to voltmeter script and sys_id. added output of adc and filtered voltage to sys_id

--- HIVE/motion/arduino/simple_voltmeter/simple_voltmeter.ino
+++ HIVE/motion/arduino/simple_voltmeter/simple_voltmeter.ino
@@ -1,7 +1,13 @@
+// voltmeter with double lowpass filter
+
 #define ANALOG_IN_PIN A0
 
 float divider_voltage = 0.0;
 float device_voltage = 0.0;
+float device_voltage_filt1 = 0.0;
+float device_voltage_filt2 = 0.0;
+const float tau1 = 64;
+const float tau2 = 128;
 
 float R1 = 22000.0;
 float R2 = 22000.0;
@@ -11,7 +17,13 @@ float ref_voltage = 5.0;
 int adc_val = 0;
 float adc_filt = 0.0;
 
-int interval = 50;
+// TIMING TABLE //
+// # Filters | Read Interval (ms) //
+// 0 | 5
+// 1 | 15
+// 2 | 35
+// doing double filter, run at 35 ms
+int interval = 35;
 
 long currentMillis = 0;
 long previousMillis = 0;
@@ -27,18 +39,26 @@ void loop(){
 
         // get adc analog value
         adc_val = analogRead(ANALOG_IN_PIN);
-        adc_filt = adc_filt + (float(adc_val)- adc_filt)/32;
-        Serial.print(adc_val);
-        Serial.print(", ");
-        Serial.print(adc_filt);
-        Serial.print(", ");
-        Serial.println("");
+//        adc_filt = adc_filt + (float(adc_val)- adc_filt)/32;
+//        Serial.print(adc_val);
+//        Serial.print(", ");
+//        Serial.print(adc_filt);
+//        Serial.print(", ");
+//        Serial.println("");
 
         // compute voltage at the divider
         divider_voltage = adc_val/1024.0*ref_voltage;
 
         // compute voltage at device
         device_voltage = divider_voltage/(R2/(R1+R2));
+        device_voltage_filt1 = device_voltage_filt1 + (device_voltage - device_voltage_filt1)/tau1;
+        device_voltage_filt2 = device_voltage_filt2 + (device_voltage_filt1 - device_voltage_filt2)/tau2;
+        Serial.print(device_voltage);
+        Serial.print(", ");
+        Serial.print(device_voltage_filt1);
+        Serial.print(", ");
+        Serial.print(device_voltage_filt2);
+        Serial.println(", ");
 
         // print voltage
 //        Serial.print(device_voltage);
--- HIVE/motion/arduino/system_id/sys_id/sys_id.ino
+++ HIVE/motion/arduino/system_id/sys_id/sys_id.ino
@@ -13,11 +13,13 @@ TO ADD
 
 float divider_voltage = 0.0;
 float device_voltage = 0.0;
+float device_voltage_filt = 0.0;
+float tau = 64;
 
 float R1 = 22000.0;
 float R2 = 22000.0;
 
-float ref_voltage = 5.0;
+float ref_voltage = 4.096;
 
 int adc_val = 0;
 
@@ -49,6 +51,7 @@ int motorInterval = 10000;
 
 int command_step = 0;
 int command_val = 0;
+
 //////////////////////////////////
 //     ROTARY ENCODER SETUP     //
 //////////////////////////////////
@@ -62,35 +65,26 @@ int command_val = 0;
 volatile long encoder_count = 0;
 
 // interval for measurements
-int interval = 500;
+int sensorInterval = 100;
 
 // time tracking
-long previousMillis = 0;
-long currentMillis = 0;
+long prev_sensorTimer = 0;
+long curr_sensorTimer = 0;
 
 // motor rpm
 int rpm = 0;
 
-// /////////////////////////////////////
-// //        serial comm vars         //
-// /////////////////////////////////////
-//
-// const byte msg_length = 16;       // max message size
-// char chars_received[msg_length];   // char array to store message
-// char temp_chars[msg_length];       // char array for parsing
-//
-// // hold parsed data
-// char char_from_msg[msg_length] = {0};
-// int int_from_msg = 0;
-//
-// boolean new_data = false;
-
 void setup() {
     Serial.begin(9600);
     Serial.println("PROGRAM: sys_id.ino");
-    Serial.println("UPLOAD DATE: 2022 APR 08");
+    Serial.println("UPLOAD DATE: 2022 APR 13");
 
-    Serial.println("Command Value, RPM, Motor Voltage");
+    Serial.println("Command Value, RPM, ADC, Motor Voltage (filtered)");
+
+    //////////////////////////////////
+    //      VOLTMETER EXTERNAL      //
+    //////////////////////////////////
+    analogReference(EXTERNAL);
 
     //////////////////////////////////
     //      MOTOR DRIVER SETUP      //
@@ -109,32 +103,15 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(ENC_INA), updateEncoder, RISING);
 
     // set initial time
-    previousMillis = millis();
-
+    prev_sensorTimer = millis();
+    prev_motorTimer = millis();
 
 }
 
 void loop(){
-    // ///////////////////////////////
-    // // SERIAL COM / MOTOR DRIVER //
-    // ///////////////////////////////
-    // receive_message();
-    // if (new_data == true) {
-    //     // temporary copy to preserve original data
-    //     // bc strtok() in parse_msg() replaces commas with \0
-    //     strcpy(temp_chars, chars_received);
-    //     parse_msg();
-    //     show_parsed_msg();
-    //     send_commands();
-    //     new_data = false;
-    // }
-
-    currentMillis = millis();
-    if (currentMillis - previousMillis > interval) {
-        previousMillis = currentMillis;
-
-        Serial.print(command_val);
-        Serial.print(", ");
+    curr_sensorTimer = millis();
+    if (curr_sensorTimer - prev_sensorTimer > sensorInterval) {
+        prev_sensorTimer = curr_sensorTimer;
 
         /////////////////////////////
         //     ROTARY ENCODER      //
@@ -142,10 +119,6 @@ void loop(){
 
         // calc rpm
         rpm = (float)(encoder_count * 60 / ENC_REV_COUNT);
-
-        // print rpm
-        Serial.print(rpm);
-        Serial.print(", ");
 
         // reset encoder count
         encoder_count = 0;
@@ -160,11 +133,23 @@ void loop(){
         // compute voltage at the divider
         divider_voltage = adc_val/1024.0*ref_voltage;
 
-        // compute voltage at device
+        // compute voltage at device and filter it
         device_voltage = divider_voltage/(R2/(R1+R2));
+        device_voltage_filt = device_voltage_filt + (device_voltage - device_voltage_filt)/tau;
 
-        // print voltage
-        Serial.println(device_voltage);
+        ////////////////
+        // PRINT DATA //
+        ////////////////
+        Serial.print(command_val);
+        Serial.print(", ");
+        Serial.print(rpm);
+        Serial.print(", ");
+        Serial.print(adc_val);
+        Serial.print(", ");
+        Serial.print(device_voltage_filt);
+        Serial.print(", ");
+        Serial.println("");
+
     } // end rpm/voltage/text-output loop
 
     // do motor command steps
@@ -195,88 +180,3 @@ void loop(){
 void updateEncoder() {
     encoder_count ++;
 }
-
-
-// ///////////////////////////
-// // serial comm functions //
-// ///////////////////////////
-// void receive_message() {
-//     static boolean recv_in_progress = false;
-//     static byte buffer_index = 0;
-//     char start_marker = '<';
-//     char end_marker = '>';
-//     char current_char;
-//
-//     while (Serial.available() > 0 && new_data == false) {
-//         current_char = Serial.read();
-//
-//         if (recv_in_progress == true) {
-//             if (current_char != end_marker) {
-//                 chars_received[buffer_index] = current_char;
-//                 buffer_index++;
-//                 if (buffer_index >= msg_length) {
-//                     buffer_index = msg_length - 1;
-//                 }
-//             }
-//             else {
-//                 chars_received[buffer_index] = '\0'; // terminate string
-//                 recv_in_progress = false;
-//                 buffer_index = 0;
-//                 new_data = true;
-//             }
-//         }
-//         else if (current_char == start_marker) {
-//             recv_in_progress = true;
-//         }
-//     }
-// }
-//
-// void parse_msg() {
-//     char * strtok_index; // index for strtok()
-//
-//     strtok_index = strtok(temp_chars,","); // get string
-//     strcpy(char_from_msg, strtok_index);  // copy string to char_from_msg
-//
-//     strtok_index = strtok(NULL, ",");     // continuing from previous indx, get int
-//     int_from_msg = atoi(strtok_index);   // convert to int
-// }
-//
-// void show_parsed_msg() {
-//     Serial.print("act_id: ");
-//     Serial.print(char_from_msg);
-//     Serial.print("\t");
-//     Serial.print("act_val: ");
-//     Serial.print(int_from_msg);
-//     Serial.print("\t");
-// }
-//
-// void send_commands() {
-//     // process and send actuation commands
-//     switch (char_from_msg[0]) {
-//         case 'L':                                   // 'L' = left tread
-//             // left tread
-//             tread_left.drive(int_from_msg);
-//             break;
-//         case 'R':                                   // 'R' = right tread
-//             // right tread
-//             tread_right.drive(int_from_msg);
-//             break;
-//         case 'B':                                   // 'B' = brake
-//             tread_left.brake();
-//             tread_right.brake();
-//         case 'S':                                   // 'S' = standby
-//             if (int_from_msg == 0) {
-//                 // standby = false so turn on
-//                 tread_left.standby(LOW);
-//                 tread_right.standby(LOW);
-//             } else if (int_from_msg == 1) {
-//                 // standby = true so turn off
-//                 tread_left.standby(HIGH);
-//                 tread_right.standby(HIGH);
-//             }
-//         default:
-//             // if unknown actuator id is received, brake and standby
-//             tread_left.brake();
-//             tread_right.brake();
-//     }
-// }
