79a60c53c9841eae1929e0e26334c7adea2ccc77
brandon <brandon05250@gmail.com>
2022-04-24 14:00:51 -0400

    migrated test code into path_functions.py

--- HIVE/controller_sim/path_functions.py
+++ HIVE/controller_sim/path_functions.py
@@ -67,3 +67,43 @@ def find_target(point,path,proj_dist):
 
 
     return target, closest
+
+
+if __name__ == "__main__":
+    # 1 get normal point
+    # 2 find target
+    test = 2
+    if test == 1:
+        # path = np.asarray([[2,2],[3,6],[7,7],[7,0],[-2,-2],[3,-2]])
+        path = np.array([[1,4],[-10,4]])
+        path = path.T
+
+        P = np.array([2,2])
+        A = np.array([-1,1])
+        B = np.array([3,1])
+        print(get_normal_point(P,A,B))
+
+        P = np.array([4,2])
+        print(get_normal_point(P,A,B))
+
+        P = np.array([2,2])
+        A = np.array([-1,1])
+        B = np.array([5,-5])
+        print(get_normal_point(P,A,B))
+
+    elif test == 2:
+        points =     np.array([[4,2], [3,8],   [10,5],    [11,1]]).T
+        expected_T = np.array([[2,3], [5,5.8], [8.3,2.5], [10,0]]).T
+        expected_C = np.array([[2,2], [4,5.5], [7.5,3.3], [9.5,0.5],]).T
+        path = np.array([[0,0],[2,2],[2,5],[6,6],[10,0]])
+        path = path.T
+        proj_dist = 1
+
+        for k in range(max(points.shape)):
+            point = points[:,k]
+            t,c = find_target(point,path,proj_dist)
+            # t,c = my_hive.XXfind_target(point,path,proj_dist)
+            print("P: %.1f,%.1f \t T: %.1f,%.1f \t eT: %.1f,%.1f \t C: %.1f,%.1f \t eC: %.1f,%.1f" %
+                (point[0],point[1],
+                t[0],t[1],expected_T[0,k],expected_T[1,k],
+                c[0],c[1],expected_C[0,k],expected_C[1,k]))
