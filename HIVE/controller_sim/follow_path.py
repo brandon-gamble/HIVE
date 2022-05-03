7e7e8c2baeeaef40f4e46380051e6436fe72dce4
brandon <brandon05250@gmail.com>
2022-05-02 20:31:13 -0400

    GOT PATH FOLLOWING SIMULATION TO WORK. NO ANIMATION

--- HIVE/controller_sim/follow_path.py
+++ HIVE/controller_sim/follow_path.py
@@ -1,3 +1,15 @@
+'''
+TO DO
+- change saturation from speed to individual motor omegas
+    - self.omega_max_l and _r (from test data)
+- things that will change with better test data:
+    - controller gains
+    - sys id variables of motor
+        -> MAKE IT CLEAR WHERE THESE VALUES PLUG IN
+
+
+'''
+
 import path_functions as path_fn
 import numpy as np
 
@@ -5,6 +17,21 @@ class HIVE:
     def __init__(self,num_timesteps,L,R,startpos):
         self.meter2pixel = 3779.52
         self.rpm2radsec = 0.10472
+
+        #########################
+        # motor characteristics #
+        #########################
+        self.max_speed = 2 # [m/s]
+
+        self.J_l = 0.1
+        self.k_l = 2
+        self.b_l = 1
+        self.omega_max_l = []
+
+        self.J_r = 0.1
+        self.k_r = 2
+        self.b_r = 1
+        self.omega_max_r = []
 
         ################################
         #  dims, pos/pose, velocities  #
@@ -49,129 +76,165 @@ class HIVE:
         self.omega_l_measured = 0
         self.omega_r_measured = 0
 
-    def XXget_normal_point(self,P,A,B):
-        # find point on line segment closest to a given point
-        # P is point
-        # A is starting point of segment
-        # B is end point of segment
+    def simulate_follow(self,waypoints,
+                            n_steps,T_phys,T_cont,
+                            kp_heading,PI_left,PI_right,
+                            proj_dist):
+        ratio = T_cont/T_phys
 
-        a = P - A
-        b = B - A
-        c = P - B
+        # proportional and integral gains for left motor
+        kp_l = PI_left[0]
+        ki_l = PI_left[1]
 
-        normal_point = A + np.dot(a,b)/np.linalg.norm(b)**2*b;
+        # proportional and integral gains for right motor
+        kp_r = PI_right[0]
+        ki_r = PI_right[1]
 
-        x_between_AB = (normal_point[0]>=min(A[0],B[0]) and normal_point[0]<=max(A[0],B[0]))
-        y_between_AB = (normal_point[1]>=min(A[1],B[1]) and normal_point[1]<=max(A[1],B[1]))
+        # heading controller is simple propotional with kp_heading
 
-        if x_between_AB and y_between_AB:
-            return normal_point
-        else:
-            norm_to_A = np.linalg.norm(a)
-            norm_to_B = np.linalg.norm(c)
-            if norm_to_A < norm_to_B:
-                normal_point = A;
-            else:
-                normal_point = B;
+        # initialize motor control inputs at 0
+        u_integral_l = 0
+        u_integral_r = 0
 
-        return normal_point
+        # initialize control actuations at 0
+        u_l = 0
+        u_r = 0
 
-    def XXfind_target(self,point,path,proj_dist):
-        N_pts = max(path.shape)
-        N_seg = N_pts - 1
+        for k in range(n_steps-1):
+            ##############
+            # CONTROLLER #
+            ##############
+            # evaluate controller every time k is a multiple of the
+            # ratio of time scales of physics and controller
+            if (k+1) % ratio == 0:
+                #######################################
+                # pretend to take sensor measurements #
+                #######################################
+                self.x_measured = self.x[k]
+                self.y_measured = self.y[k]
+                self.phi_measured = self.phi[k]
+                self.omega_l_measured = self.omega_l[k]
+                self.omega_r_measured = self.omega_r[k]
 
-        normal_vecs = np.zeros([2,N_seg])     # normal vec to each line segment in path
-        normal_norms = np.zeros([N_seg])    # length of normal vecs^
+                # find target
+                P = [self.x_measured, self.y_measured]
+                self.target, self.closest = path_fn.find_target(P,waypoints,proj_dist)
+                targ_x = self.target[0]
+                targ_y = self.target[1]
 
-        for k in range(N_seg):
-            normal_vecs[:,k] = self.XXget_normal_point(point,path[:,k],path[:,k+1]) - point
-            normal_norms[k] = np.linalg.norm(normal_vecs[:,k])
+                # velocity vector and desired speed/heading
+                u = np.array([[targ_x - self.x_measured],[targ_y - self.y_measured]])
+                s_des = min(np.linalg.norm(u),self.max_speed)
+                phi_des = np.arctan2(u[1],u[0])
 
-        index = np.argmin(normal_norms)     # index of smallest norm
-        Q = normal_vecs[:,index]            # smallest normal vec
-        closest = point + Q                 # closest point on path
-        R = path[:,index+1]-closest         # distance from C to end of seg [index]
-        norm_R = np.linalg.norm(R)          # get norm of R
-        unit_R = R/norm_R                   # get unit vec
+                # error in heading
+                error_phi = phi_des - self.phi_measured
+                # need to use atan2 to unwind error
+                error_phi = np.arctan2(np.sin(error_phi),np.cos(error_phi))
 
-        if norm_R >= proj_dist:
-            target = closest + unit_R*proj_dist
-        else:
-            proj_remain = proj_dist - norm_R
-            seg = index+1
-            while proj_remain > 0:
-                if seg == N_seg:                # if looking past last segment, terminate at endpoint
-                    target = path[:,N_seg]
-                    return target, closest
-                else:
-                    this_seg = path[:,seg+1] - path[:,seg]
-                    this_norm = np.linalg.norm(this_seg)
-                    unit_vec = this_seg/this_norm
-                    if proj_remain < this_norm: # project remaining along seg
-                        this_proj = unit_vec*proj_remain
-                        proj_remain = 0
-                        target = path[:,seg] + this_proj
-                    else: # project fully along seg and go to next seg
-                        proj_remain = proj_remain - this_norm
+                # proportional control
+                omega_des = error_phi*kp_heading
+
+                # motor setpoints
+                omega_r_des = (s_des + omega_des*self.L)/self.R
+                omega_l_des = (s_des - omega_des*self.L)/self.R
+
+                # motor left control
+                error_l = omega_l_des - self.omega_l_measured
+                u_integral_l = u_integral_l + error_l*T_cont
+                u_l = kp_l*error_l + ki_l*u_integral_l
+
+                # motor right control
+                error_r = omega_r_des - self.omega_r_measured
+                u_integral_r = u_integral_r + error_r*T_cont
+                u_r = kp_r*error_r + ki_r*u_integral_r
 
 
-        return target, closest
+            # end of controller loop
 
-    def simulate_follow(self,T_phys,T_cont,path):
+            ###############
+            #    PLANT    #
+            ###############
+            self.omega_l[k+1] = self.omega_l[k] + T_phys/self.J_l*(self.k_l*u_l-self.b_l*self.omega_l[k])
+            self.omega_r[k+1] = self.omega_r[k] + T_phys/self.J_r*(self.k_r*u_r-self.b_r*self.omega_r[k])
+
+            self.s[k+1] = (self.omega_l[k+1]+self.omega_r[k+1])*self.R/2
+            self.omega[k+1] = (self.omega_r[k+1]-self.omega_l[k+1])*self.R/2/self.L
+            self.phi[k+1] = self.phi[k] + self.omega[k]*T_phys
+            self.x[k+1] = self.x[k] + self.s[k]*np.cos(self.phi[k])*T_phys
+            self.y[k+1] = self.y[k] + self.s[k]*np.sin(self.phi[k])*T_phys
+
+            self.log_targ[0,k] = self.target[0]
+            self.log_targ[1,k] = self.target[1]
+
+            self.log_closest[0,k] = self.closest[0]
+            self.log_closest[1,k] = self.closest[1]
+
         return
-
-
 
 if __name__ == "__main__":
 
-    # time step of physics and controller
-    T_phys = 0.001
-    T_cont = 0.01
-    ratio = T_cont/T_phys
+    import matplotlib.pyplot as plt
 
-    # time vector for simulation
-    t = np.arange(0,15,T_phys)
-    n = len(t)
+    #   1   mimic HW4pathFollowing_tight_corner from autonomy
+    #   2
+    test = 1
 
-    my_hive = HIVE(n,0.1,0.1,[0,0])
-
-    # 1 get normal point
-    # 2 find target
-    test = 2
     if test == 1:
-        # path = np.asarray([[2,2],[3,6],[7,7],[7,0],[-2,-2],[3,-2]])
-        path = np.array([[1,4],[-10,4]])
-        path = path.T
 
-        P = np.array([2,2])
-        A = np.array([-1,1])
-        B = np.array([3,1])
-        print(path_fn.get_normal_point(P,A,B))
+        # time step of physics and controller
+        T_phys = 0.001
+        T_cont = 0.01
+        ratio = T_cont/T_phys
 
-        P = np.array([4,2])
-        print(path_fn.get_normal_point(P,A,B))
+        # time vector for simulation
+        max_time = 15
+        t = np.arange(0,max_time,T_phys)
+        n = len(t)
 
-        P = np.array([2,2])
-        A = np.array([-1,1])
-        B = np.array([5,-5])
-        print(path_fn.get_normal_point(P,A,B))
+        my_hive = HIVE(n,0.1,0.1,[1,4])
+
+        waypoints = np.array([[2,2], [3,6], [7,7], [7,0], [-2,-2],[3,-2]]).T
+
+        my_hive.simulate_follow(waypoints,
+                                n,T_phys,T_cont,
+                                kp_heading=2,
+                                PI_left=[1/3, 10/3],
+                                PI_right=[1/3, 10/3],
+                                proj_dist=2)
+
+        # plt.scatter(waypoints[0,:], waypoints[1,:])
+        # plt.plot(my_hive.x,my_hive.y)
+
+        # plot waypoints and path
+        plt.subplot(1,3,1)
+        plt.scatter(waypoints[0,:],waypoints[1,:],label='waypoints')
+        plt.plot(my_hive.x,my_hive.y,label='HIVE path')
+        plt.title('HIVE path')
+        plt.legend()
+
+        # plot states x,y,phi thru time
+        plt.subplot(1,3,2)
+        plt.plot(t,my_hive.x,label='x')
+        plt.plot(t,my_hive.y,label='y')
+        plt.plot(t,my_hive.phi,label='phi')
+        plt.legend()
+
+        # plot states omega and s thru time
+        plt.subplot(1,3,3)
+        plt.plot(t,my_hive.omega,label='omega')
+        # plt.plot(t,my_hive.omega_l,label='omega_l')
+        # plt.plot(t,my_hive.omega_r,label='omega_r')
+        plt.plot(t,my_hive.s,label='s')
+        plt.legend()
+
+        plt.show()
+
+
+
 
     elif test == 2:
-        points =     np.array([[4,2], [3,8],   [10,5],    [11,1]]).T
-        expected_T = np.array([[2,3], [5,5.8], [8.3,2.5], [10,0]]).T
-        expected_C = np.array([[2,2], [4,5.5], [7.5,3.3], [9.5,0.5],]).T
-        path = np.array([[0,0],[2,2],[2,5],[6,6],[10,0]])
-        path = path.T
-        proj_dist = 1
-
-        for k in range(max(points.shape)):
-            point = points[:,k]
-            t,c = path_fn.find_target(point,path,proj_dist)
-            # t,c = my_hive.XXfind_target(point,path,proj_dist)
-            print("P: %.1f,%.1f \t T: %.1f,%.1f \t eT: %.1f,%.1f \t C: %.1f,%.1f \t eC: %.1f,%.1f" %
-                (point[0],point[1],
-                t[0],t[1],expected_T[0,k],expected_T[1,k],
-                c[0],c[1],expected_C[0,k],expected_C[1,k]))
+        a = []
 
     # elif test == 3:
     # elif test == 4:
