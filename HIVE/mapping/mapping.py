3a64563af921fa10434e24f3e87b0cf3b6aab58f
brandon <brandon05250@gmail.com>
2024-03-08 10:56:58 -0500

    mapping.py: align color and depth frames. plot depth and plot slice

--- HIVE/mapping/mapping.py
+++ HIVE/mapping/mapping.py
@@ -8,12 +8,16 @@ import imutils
 import sys
 
 ######################################
+'''
 need to flip origin of image
     should probably put get_curr_frame in its own camera doc to import
     that way changes work across docs (no unique version of get frame)
     should probably flip origin in get_curr_frame, but this may break the indexing in the aruco tracking
-######################################
 
+    ** zero pixels result from "shadow" so instead of averaging to defeat zeros,
+    ** need to IGNORE zeros.
+'''
+######################################
 def get_curr_frame(pipeline):
     # Wait for a coherent pair of frames: depth and color
     frames = pipeline.wait_for_frames()
@@ -28,18 +32,25 @@ def get_curr_frame(pipeline):
     # depth: <class 'numpy.ndarray'>
     # color: <class 'numpy.ndarray'>
 
-    ############################################
-    # print(depth_frame.get_distance(10,10))
-    # print(depth_image[10,10])
-    ############################################
-
-    # # Stack into RGBD image
-    # RGBD_image = np.dstack((color_image,depth_image))
-
     frame_pair = (color_frame, depth_frame)
     image_pair = (color_image, depth_image)
 
     return image_pair
+
+def get_aligned_frame(pipeline):
+    align = rs.align(rs.stream.depth)
+
+    frames = pipeline.wait_for_frames()
+    frames = align.process(frames)
+
+    aligned_color_frame = frames.get_color_frame()
+
+    color_image = np.asanyarray(aligned_color_frame.get_data())
+    depth_image = np.asanyarray(frames.get_depth_frame().get_data())
+
+    pair = (color_image, depth_image)
+
+    return pair
 
 def get_z_slice(depth_image, slice_height, slice_size):
     # depth_image:  array of depth data
@@ -60,8 +71,10 @@ def get_z_slice(depth_image, slice_height, slice_size):
 
     return z_slice
 
-def plot_image(image):
+def plot_image_3d(image):
     # "image" is array, not frame
+
+    image = np.flip(image, 0)
 
     height = image.shape[0]
     width = image.shape[1]
@@ -71,6 +84,8 @@ def plot_image(image):
     X, Y = np.meshgrid(x, y)
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
 
     surf = ax.plot_surface(X, Y, image, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
@@ -80,7 +95,23 @@ def plot_image(image):
 
     return
 
+def plot_image_2d(image):
+    # "image" is array, not frame
+    plt.imshow(image, cmap='hsv')
+    plt.colorbar()
+    plt.show()
+    return
+
 def main():
+    '''
+    test files
+    ---------------------------
+    01  plot color map of depth image, then aligned color image.
+        note that color image has smaller FOV, so when aligned has large black border
+    02  plot 2d color map, 3d color map, then plot slices from map
+    '''
+    test_case = 2
+
     # Configure depth and color streams
     pipeline = rs.pipeline()
     config = rs.config()
@@ -90,23 +121,39 @@ def main():
     # Start streaming
     pipeline.start(config)
 
-    while True:
-        #ui = input("new frame?")
-        # if ui == "plot":
-            # plt.show()
-        image_pair = get_curr_frame(pipeline)
-        depth_image = image_pair[1]
+    if test_case == 1:
+        while True:
+            #image_pair = get_curr_frame(pipeline)
+            image_pair = get_aligned_frame(pipeline)
+            depth_image = image_pair[1]
+            color_image = image_pair[0]
 
-        z_slice_120 = get_z_slice(depth_image, 120, 30)
-        z_slice_240 = get_z_slice(depth_image, 240, 30)
-        z_slice_360 = get_z_slice(depth_image, 360, 30)
+            plot_image_2d(depth_image)
+            plt.imshow(color_image)
+            plt.show()
 
-        # plt.plot(z_slice_120)
-        # plt.plot(z_slice_240)
-        # plt.plot(z_slice_360)
+    elif test_case == 2:
+        while True:
+            image_pair = get_aligned_frame(pipeline)
+            depth_image = image_pair[1]
 
-        plot_image(depth_image)
+            plot_image_2d(depth_image)
+            plot_image_3d(depth_image)
 
+            a = 200
+            b = 370
+            c = 420
+
+            z_slice_a = get_z_slice(depth_image, a, 2)
+            z_slice_b = get_z_slice(depth_image, b, 2)
+            z_slice_c = get_z_slice(depth_image, c, 2)
+
+            plt.plot(z_slice_a, label=str(a))
+            plt.plot(z_slice_b, label=str(b))
+            plt.plot(z_slice_c, label=str(c))
+
+            plt.legend()
+            plt.show()
 
 
 
