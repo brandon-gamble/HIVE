b0cd1ff342697f64f2ddffa7d561006cb58e860b
brandon <brandon05250@gmail.com>
2024-03-22 16:02:56 -0400

    joystick commands to xbee

---
+++ HIVE/relay/joystick_led_control/joystick/joystick.ino
@@ -0,0 +1,23 @@
+#define joyX A0
+#define joyY A1
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  int xValue = analogRead(joyX);
+  int yValue = analogRead(joyY);
+ 
+  //print the values with to plot or view
+//  Serial.print(xValue);
+//  Serial.print("\t");
+//  Serial.println(yValue);
+
+  int ledValue = map(xValue, 0, 1023, 0,255);
+  String ledCommand = "<" + String(ledValue) + ">";
+  Serial.println(ledCommand);
+  delay(20); // at 9600 baud, cannot decrease delay less than 20ms
+
+}
